912a25ae7dc3b40bc37d0991610b22b9dbbe31b0
chependuk16 <chependuk16@gmail.com>
2023-09-20 18:37:24 +0300

    work with WelcomeViewController logic

--- LoginAppAnalysis/WelcomeViewController.swift
+++ LoginAppAnalysis/WelcomeViewController.swift
@@ -7,23 +7,42 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+final class WelcomeViewController: UIViewController {
 
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var user = ""
+    
+    private let primaryColor = UIColor(
+        red: 200/255,
+        green: 100/255,
+        blue: 130/255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        red: 22/255,
+        green: 144/255,
+        blue: 103/255,
+        alpha: 1
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        welcomeLabel.text = "Welcome, \(user)!"
     }
-    
+  
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - Set background color
+extension UIView {
+    func addGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
-    */
-
 }
